f69bb127e5dbd81ee8fb29d25424c293f2a3c60f
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2022-12-22 16:57:50 -0600

    Delete GetOS_woWMI.ps1

--- GetOS_woWMI.ps1
+++
@@ -1,3 +0,0 @@
-﻿Get-ADComputer -Filter * -Properties Name, OperatingSystem | Select-Object Name, OperatingSystem | Tee-Object -Variable results | 
-Export-CSV -Path ".\OSReport-$((get-addomain).forest)-$(Get-Date -Format MMddyyyy_HHmmss).csv"
-$results
