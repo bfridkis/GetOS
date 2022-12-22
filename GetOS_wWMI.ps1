bed00d6acdc15674e7ac6783e116a2183f822bd7
Benjamin Fridkis <32713413+bfridkis@users.noreply.github.com>
2022-12-22 16:56:03 -0600

    Add files via upload

---
+++ GetOS_wWMI.ps1
@@ -0,0 +1,16 @@
+﻿$comps = New-Object System.Collections.Generic.List[System.Object]
+Get-ADObject -LDAPFilter "(objectClass=computer)" | ForEach-Object {
+    $currComp = $_.name
+    Try {
+        $comps.add([PSCustomObject]@{"Hostname"=$_.name;
+                                     "OS"="$($(Get-WmiObject -ComputerName $_.name -Class 'Win32_OperatingSystem' -ErrorAction Stop).Caption)"
+                                   })
+    }
+    Catch {
+        $comps.add([PSCustomObject]@{"Hostname"=$currComp;
+                                     "OS"="Unavailable. Exception: $($_.Exception.Message)"
+                                     })
+    }
+}
+
+$comps | Select-Object
---
+++ GetOS_woWMI.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADComputer -Filter * -Properties Name, OperatingSystem | Select-Object Name, OperatingSystem | Tee-Object -Variable results | 
+Export-CSV -Path ".\OSReport-$((get-addomain).forest)-$(Get-Date -Format MMddyyyy_HHmmss).csv"
+$results
